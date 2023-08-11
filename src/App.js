4a72777cc1115f863069a4d4d6756ab60d122700
shekhar agrawal <csa.jpr08@gmail.com>
2023-08-11 20:34:23 +0530

    creating first version of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
+import Vans from "./pages/Vans";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <Link to="/" className="site-logo">
+          #VANLIFE
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
